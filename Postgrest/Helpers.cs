852dd54173d5f7760b1c12532557d8bda1d10a65
Louis DeScioli <louis.descioli@gmail.com>
2024-04-26 00:38:30 -0400

    Creates UnityWebRequest subroutine for MakeRequest helper

--- Postgrest/Helpers.cs
+++ Postgrest/Helpers.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using UnityEngine.Networking;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -19,142 +21,209 @@ using Supabase.Postgrest.Responses;
 namespace Supabase.Postgrest
 {
 
-	internal static class Helpers
-	{
-		private static readonly HttpClient Client = new HttpClient();
+    internal static class Helpers
+    {
+        private static readonly HttpClient Client = new HttpClient();
 
-		private static readonly Guid AppSession = Guid.NewGuid();
+        private static readonly Guid AppSession = Guid.NewGuid();
 
-		/// <summary>
-		/// Helper to make a request using the defined parameters to an API Endpoint and coerce into a model. 
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="clientOptions"></param>
-		/// <param name="method"></param>
-		/// <param name="url"></param>
-		/// <param name="data"></param>
-		/// <param name="headers"></param>
-		/// <param name="serializerSettings"></param>
-		/// <param name="getHeaders"></param>
-		/// <param name="cancellationToken"></param>
-		/// <returns></returns>
-		public static async Task<ModeledResponse<T>> MakeRequest<T>(ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
-			Dictionary<string, string>? headers = null, Func<Dictionary<string, string>>? getHeaders = null, CancellationToken cancellationToken = default) where T : BaseModel, new()
-		{
-			var baseResponse = await MakeRequest(clientOptions, method, url, serializerSettings, data, headers, cancellationToken);
-			return new ModeledResponse<T>(baseResponse, serializerSettings, getHeaders);
-		}
+        /// <summary>
+        /// Helper to make a request using the defined parameters to an API Endpoint and coerce into a model.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="clientOptions"></param>
+        /// <param name="method"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializerSettings"></param>
+        /// <param name="getHeaders"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<ModeledResponse<T>> MakeRequest<T>(ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
+            Dictionary<string, string>? headers = null, Func<Dictionary<string, string>>? getHeaders = null, CancellationToken cancellationToken = default) where T : BaseModel, new()
+        {
+            var baseResponse = await MakeRequest(clientOptions, method, url, serializerSettings, data, headers, cancellationToken);
+            return new ModeledResponse<T>(baseResponse, serializerSettings, getHeaders);
+        }
 
-		/// <summary>
-		/// Helper to make a request using the defined parameters to an API Endpoint.
-		/// </summary>
-		/// <param name="clientOptions"></param>
-		/// <param name="method"></param>
-		/// <param name="url"></param>
-		/// <param name="data"></param>
-		/// <param name="headers"></param>
-		/// <param name="serializerSettings"></param>
-		/// <param name="cancellationToken"></param>
-		/// <returns></returns>
-		public static async Task<BaseResponse> MakeRequest(ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
-			Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
-		{
-			var builder = new UriBuilder(url);
-			var query = HttpUtility.ParseQueryString(builder.Query);
+        /// <summary>
+        /// Helper to make a request using the defined parameters to an API Endpoint.
+        /// </summary>
+        /// <param name="clientOptions"></param>
+        /// <param name="method"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <param name="headers"></param>
+        /// <param name="serializerSettings"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<BaseResponse> MakeRequest(ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
+            Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+        {
+            var builder = new UriBuilder(url);
+            var query = HttpUtility.ParseQueryString(builder.Query);
 
-			if (data != null && method == HttpMethod.Get)
-			{
-				// Case if it's a Get request the data object is a dictionary<string,string>
-				if (data is Dictionary<string, string> reqParams)
-				{
-					foreach (var param in reqParams)
-						query[param.Key] = param.Value;
-				}
-			}
+            if (data != null && method == HttpMethod.Get)
+            {
+                // Case if it's a Get request the data object is a dictionary<string,string>
+                if (data is Dictionary<string, string> reqParams)
+                {
+                    foreach (var param in reqParams)
+                        query[param.Key] = param.Value;
+                }
+            }
 
-			builder.Query = query.ToString();
+            builder.Query = query.ToString();
 
-			using var requestMessage = new HttpRequestMessage(method, builder.Uri);
+#if UNITY_2022_3_OR_NEWER
+            return await MakeUnityRequest(builder.Uri, clientOptions, method, url, serializerSettings, data, headers, cancellationToken);
+#else
+            return await MakeNativeRequest(builder.Uri, clientOptions, method, url, serializerSettings, data, headers, cancellationToken);
+#endif
+        }
 
-			if (data != null && method != HttpMethod.Get)
-			{
-				var stringContent = JsonConvert.SerializeObject(data, serializerSettings);
+        private static async Task<BaseResponse> MakeNativeRequest(Uri uri, ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
+            Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+        {
+            using var requestMessage = new HttpRequestMessage(method, uri);
 
-				if (!string.IsNullOrWhiteSpace(stringContent) && JToken.Parse(stringContent).HasValues)
-				{
-					requestMessage.Content = new StringContent(stringContent, Encoding.UTF8, "application/json");
-				}
-			}
+            if (data != null && method != HttpMethod.Get)
+            {
+                var stringContent = JsonConvert.SerializeObject(data, serializerSettings);
 
-			if (headers != null)
-			{
-				foreach (var kvp in headers)
-				{
-					requestMessage.Headers.TryAddWithoutValidation(kvp.Key, kvp.Value);
-				}
-			}
+                if (!string.IsNullOrWhiteSpace(stringContent) && JToken.Parse(stringContent).HasValues)
+                {
+                    requestMessage.Content = new StringContent(stringContent, Encoding.UTF8, "application/json");
+                }
+            }
 
-			var response = await Client.SendAsync(requestMessage, cancellationToken);
-			var content = await response.Content.ReadAsStringAsync();
+            if (headers != null)
+            {
+                foreach (var kvp in headers)
+                {
+                    requestMessage.Headers.TryAddWithoutValidation(kvp.Key, kvp.Value);
+                }
+            }
 
-			if (response.IsSuccessStatusCode)
-				return new BaseResponse(clientOptions, response, content);
+            var response = await Client.SendAsync(requestMessage, cancellationToken);
+            var content = await response.Content.ReadAsStringAsync();
 
-			var exception = new PostgrestException(content)
-			{
-				Content = content,
-				Response = response,
-				StatusCode = (int)response.StatusCode
-			};
-			exception.AddReason();
-			throw exception;
-		}
+            if (response.IsSuccessStatusCode)
+                return new BaseResponse(clientOptions, response, content);
 
-		/// <summary>
-		/// Prepares the request with appropriate HTTP headers expected by Postgrest.
-		/// </summary>
-		/// <param name="method"></param>
-		/// <param name="headers"></param>
-		/// <param name="options"></param>
-		/// <param name="rangeFrom"></param>
-		/// <param name="rangeTo"></param>
-		/// <returns></returns>
-		public static Dictionary<string, string> PrepareRequestHeaders(HttpMethod method, Dictionary<string, string>? headers = null, ClientOptions? options = null, int rangeFrom = int.MinValue, int rangeTo = int.MinValue)
-		{
-			options ??= new ClientOptions();
+            var exception = new PostgrestException(content)
+            {
+                Content = content,
+                Response = response,
+                StatusCode = (int)response.StatusCode
+            };
+            exception.AddReason();
+            throw exception;
+        }
 
-			headers = headers == null ? new Dictionary<string, string>(options.Headers) : options.Headers.MergeLeft(headers);
+        private static async Task<BaseResponse> MakeUnityRequest(Uri uri, ClientOptions clientOptions, HttpMethod method, string url, JsonSerializerSettings serializerSettings, object? data = null,
+            Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+        {
+            using var request = new UnityWebRequest(uri.ToString(), method.ToString());
+            request.downloadHandler = new DownloadHandlerBuffer();
 
-			if (!string.IsNullOrEmpty(options.Schema))
-			{
-				headers.Add(method == HttpMethod.Get ? "Accept-Profile" : "Content-Profile", options.Schema);
-			}
+            if (data != null && method != HttpMethod.Get)
+            {
+                var stringContent = JsonConvert.SerializeObject(data, serializerSettings);
 
-			if (rangeFrom != int.MinValue)
-			{
-				var formatRangeTo = rangeTo != int.MinValue ? rangeTo.ToString() : null;
+                if (!string.IsNullOrWhiteSpace(stringContent) && JToken.Parse(stringContent).HasValues)
+                {
+                    request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(stringContent));
+                    request.SetRequestHeader("Content-Type", "application/json");
+                }
+            }
 
-				headers.Add("Range-Unit", "items");
-				headers.Add("Range", $"{rangeFrom}-{formatRangeTo}");
-			}
+            if (headers != null)
+            {
+                foreach (var kvp in headers)
+                {
+                    request.SetRequestHeader(kvp.Key, kvp.Value);
+                }
+            }
 
-			if (!headers.ContainsKey("X-Client-Info"))
-			{
-				try
-				{
-					// Default version to match other clients
-					// https://github.com/search?q=org%3Asupabase-community+x-client-info&type=code
-					headers.Add("X-Client-Info", $"postgrest-csharp/{Util.GetAssemblyVersion(typeof(Client))}");
-				}
-				catch (Exception)
-				{
-					// Fallback for when the version can't be found
-					// e.g. running in the Unity Editor, ILL2CPP builds, etc.
-					headers.Add("X-Client-Info", $"postgrest-csharp/session-{AppSession}");
-				}
-			}
+            var operation = request.SendWebRequest();
 
-			return headers;
-		}
-	}
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    request.Abort();
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var httpResponse = new HttpResponseMessage()
+                {
+                    StatusCode = (HttpStatusCode)request.responseCode,
+                    Content = new StringContent(request.downloadHandler.text),
+                    RequestMessage = new HttpRequestMessage(method, uri)
+                };
+                return new BaseResponse(clientOptions, httpResponse, request.downloadHandler.text);
+            }
+
+            var exception = new PostgrestException(request.downloadHandler.text)
+            {
+                Content = request.downloadHandler.text,
+                StatusCode = (int)request.responseCode
+            };
+            exception.AddReason();
+            throw exception;
+        }
+
+        /// <summary>
+        /// Prepares the request with appropriate HTTP headers expected by Postgrest.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="headers"></param>
+        /// <param name="options"></param>
+        /// <param name="rangeFrom"></param>
+        /// <param name="rangeTo"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> PrepareRequestHeaders(HttpMethod method, Dictionary<string, string>? headers = null, ClientOptions? options = null, int rangeFrom = int.MinValue, int rangeTo = int.MinValue)
+        {
+            options ??= new ClientOptions();
+
+            headers = headers == null ? new Dictionary<string, string>(options.Headers) : options.Headers.MergeLeft(headers);
+
+            if (!string.IsNullOrEmpty(options.Schema))
+            {
+                headers.Add(method == HttpMethod.Get ? "Accept-Profile" : "Content-Profile", options.Schema);
+            }
+
+            if (rangeFrom != int.MinValue)
+            {
+                var formatRangeTo = rangeTo != int.MinValue ? rangeTo.ToString() : null;
+
+                headers.Add("Range-Unit", "items");
+                headers.Add("Range", $"{rangeFrom}-{formatRangeTo}");
+            }
+
+            if (!headers.ContainsKey("X-Client-Info"))
+            {
+                try
+                {
+                    // Default version to match other clients
+                    // https://github.com/search?q=org%3Asupabase-community+x-client-info&type=code
+                    headers.Add("X-Client-Info", $"postgrest-csharp/{Util.GetAssemblyVersion(typeof(Client))}");
+                }
+                catch (Exception)
+                {
+                    // Fallback for when the version can't be found
+                    // e.g. running in the Unity Editor, ILL2CPP builds, etc.
+                    headers.Add("X-Client-Info", $"postgrest-csharp/session-{AppSession}");
+                }
+            }
+
+            return headers;
+        }
+    }
 }
